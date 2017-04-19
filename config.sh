92564e7bd9c197a4d93d8fc262f2f60d66b6eb90
Oliv4945 <oliv4945@gmail.com>
2017-04-19 11:08:22 +0200

    Add configuration file

---
+++ config.sh
@@ -0,0 +1,10 @@
+
+# Jarvis server address (hostname or IP)
+jv_pg_mqtt_server="localhost"
+
+# Jarvis server port
+jv_pg_mqtt_port=1883
+
+# Jarvis root topic to subscribe/publish, without trailing slash
+jv_pg_mqtt_topic="/Jarvis"
+
